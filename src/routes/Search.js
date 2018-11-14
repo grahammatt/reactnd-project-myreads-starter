f7137dec9f5f26deb72d05007374685837163789
Matt Graham <grahammatt@live.com>
2018-11-13 21:56:28 -0500

    feat: begin search implementation
    -bug introduced: typing fast will cause the app to crash

--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import * as BooksAPI from "../BooksAPI";
+import Book from "../components/Book";
 
 export default class Search extends Component {
+  state = {
+    foundBooks: undefined,
+    query: ''
+  };
+
+  findBooks = query => {
+    BooksAPI.search(query).then(books => {
+      this.setState({ foundBooks: books, query: query.trim()});
+    });
+  };
+
+  addBook = (bookToAdd, shelf) => {
+    BooksAPI.update(bookToAdd, shelf).then(response => {
+      console.log(response);
+    });
+  };
+
   render() {
     return (
       <div className="search-books">
@@ -11,17 +30,28 @@ export default class Search extends Component {
           </Link>
           <div className="search-books-input-wrapper">
             {/* NOTES : The search from BooksAPI is limited to a particular set of search terms.
-          *You can find these search terms here
-          *https : //github.com/udacity / reactnd - project - myreads - starter / blob / master / SEARCH_TERMS.md
-          *However,
-          *remember that the BooksAPI.search method DOES search by title or author.So,
-          *don 't worry if you don 't find a specific author or title. Every search is limited by search terms.
-          */}
-            <input type="text" placeholder="Search by title or author" />
+              *You can find these search terms here
+              *https : //github.com/udacity / reactnd - project - myreads - starter / blob / master / SEARCH_TERMS.md
+              *However,
+              *remember that the BooksAPI.search method DOES search by title or author.So,
+              *don 't worry if you don 't find a specific author or title. Every search is limited by search terms.
+            */}
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={event => this.findBooks(event.target.value)}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid" />
+          {/* Short circuting To determine if books need to be displayed */}
+          {this.state.foundBooks !== undefined && (
+            <ol className="books-grid">
+              {this.state.foundBooks.map(book => (
+                <Book key={book.id} book={book} updateBooks={this.addBook} />
+              ))}
+            </ol>
+          )}
         </div>
       </div>
     );
